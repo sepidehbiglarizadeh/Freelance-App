4c8dcbe37f6873e4ec7e138ab71738a2842210d7
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-15 20:59:17 +0330

    Add color to tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold">شروع پروژ</h1>;
+  return (
+    <h1 className="container xl:max-w-screen-xl bg-primary-900 bg-opacity-80 text-3xl font-bold">
+      شروع پروژ
+    </h1>
+  );
 }
 
 export default App;
