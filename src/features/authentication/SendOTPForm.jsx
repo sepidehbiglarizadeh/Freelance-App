83354121f9235a4c5fdce4be2cf3552e041a1b28
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-15 22:04:31 +0330

    Customize tailwind class

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -15,10 +15,10 @@ function SendOTPForm() {
             onChange={(e) => setPhoneNumber(e.target.value)}
             id="phoneNumber"
             type="text"
-            className="w-full py-3 px-4 rounded-xl text-secondary-900 border border-gray-100 outline-none hover:border-primary-500 focus:border-primary-300 focus:bg-white transition-all duration-300 ease-in-out focus:shadow-lg focus:shadow-primary-200"
+            className="textField__input"
           />
         </div>
-        <button className="px-4 py-2 font-bold bg-primary-900 text-white w-full rounded-xl transition-all duration-300 hover:bg-primary-800 shadow-lg shadow-primary-300">
+        <button className="btn btn--primary w-full">
           ارسال کد تائید
         </button>
       </form>
