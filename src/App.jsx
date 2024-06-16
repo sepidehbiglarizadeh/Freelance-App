2861d2b265d3812152ad587ae042b047c140240f
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-16 22:14:04 +0330

    Owner Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import CompleteProfile from "./pages/CompleteProfile";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import Owner from "./pages/Owner";
 import AppLayout from "./ui/AppLayout";
+import OwnerDashboard from "./pages/OwnerDashboard";
+import Projects from "./pages/Projects";
+import Project from "./pages/Project";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -19,8 +21,11 @@ function App() {
       <Routes>
         <Route path="/auth" element={<Auth />} />
         <Route path="/complete-profile" element={<CompleteProfile />} />
-        <Route element={<AppLayout />}>
-          <Route path="/owner" element={<Owner />} />
+        <Route path="/owner" element={<AppLayout />}>
+          <Route index element={<Navigate to="dashboard" replace />} />
+          <Route path="dashboard" element={<OwnerDashboard />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="projects/:id" element={<Project />} />
         </Route>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
