a9bb105aa704440fb114efa226aa12d59564d60b
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-16 10:47:35 +0330

    Send OTP handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="container xl:max-w-screen-xl">
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </div>
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <div className="container xl:max-w-screen-xl">
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import TextField from "../../ui/TextField";
+import { useMutation } from "@tanstack/react-query";
+import { getOtp } from "../../services/authService";
+import toast from "react-hot-toast";
 
 function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
 
-  const sendOTPHandler = (e) => {
+  const { isPending, error, data, mutateAsync } = useMutation({
+    mutationFn: getOtp,
+  });
+
+  const sendOTPHandler = async (e) => {
     e.preventDefault();
+    try {
+      const data = await mutateAsync({ phoneNumber });
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
   };
 
   return (
@@ -26,3 +39,7 @@ function SendOTPForm() {
 }
 
 export default SendOTPForm;
+
+// ? react-query:
+// 1- useQuery => get
+// 2- useMutation => post ,put , delete
