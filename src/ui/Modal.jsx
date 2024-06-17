f64d244a6f9598f4b45f2d7f59a543fdbf1a06eb
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-17 14:44:22 +0330

    useOutsideClick

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,11 +1,14 @@
-import React from "react";
 import { HiOutlineX } from "react-icons/hi";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 function Modal({ open, onClose, title, children }) {
+  const ref = useOutsideClick(onClose);
+
   return (
     open && (
       <div className="backdrop-blur-sm fixed top-0 left-0 w-full h-screen bg-secondary-800 bg-opacity-30 z-50">
         <div
+          ref={ref}
           className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-secondary-0 p-4 shadow-lg transition-all duration-500 ease-out 
         w-[calc(100vw-1rem)] md:max-w-lg max-h-[calc(100vh-2rem)] overflow-y-auto"
         >
