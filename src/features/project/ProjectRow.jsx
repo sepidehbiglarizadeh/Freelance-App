a4151c90c045322ac0d3d45e7a8df1102db9e052
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-17 12:37:42 +0330

    Confirm delete

--- src/features/project/ProjectRow.jsx
+++ src/features/project/ProjectRow.jsx
@@ -6,9 +6,11 @@ import truncateText from "../../utils/truncateText";
 import { TbPencilMinus } from "react-icons/tb";
 import Modal from "../../ui/Modal";
 import { useState } from "react";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 function ProjectRow({ project, index }) {
   const [isEditOpen, setIsEditopen] = useState(false);
+  const [isDeleteOpen, setIsDeleteopen] = useState(false);
 
   return (
     <Table.Row>
@@ -36,19 +38,35 @@ function ProjectRow({ project, index }) {
       </td>
       <td>
         <div className="flex items-center gap-x-4">
-          <button onClick={() => setIsEditopen(true)}>
-            <TbPencilMinus className="w-5 h-5 text-primary-900" />
-          </button>
-          <Modal
-            title="Modal Title"
-            open={isEditOpen}
-            onClose={() => setIsEditopen(false)}
-          >
-            This is Modal...
-          </Modal>
-          <button>
-            <HiOutlineTrash className="w-5 h-5 text-error" />
-          </button>
+          <>
+            <button onClick={() => setIsEditopen(true)}>
+              <TbPencilMinus className="w-5 h-5 text-primary-900" />
+            </button>
+            <Modal
+              title={`ویرایش ${project.title}`}
+              open={isEditOpen}
+              onClose={() => setIsEditopen(false)}
+            >
+              This is Modal...
+            </Modal>
+          </>
+          <>
+            <button onClick={() => setIsDeleteopen(true)}>
+              <HiOutlineTrash className="w-5 h-5 text-error" />
+            </button>
+            <Modal
+              title={`حذف ${project.title}`}
+              open={isDeleteOpen}
+              onClose={() => setIsDeleteopen(false)}
+            >
+              <ConfirmDelete
+                resourceName={project.title}
+                onClose={() => setIsDeleteopen(false)}
+                onConfirm={() => {}}
+                disabled={false}
+              />
+            </Modal>
+          </>
         </div>
       </td>
     </Table.Row>
