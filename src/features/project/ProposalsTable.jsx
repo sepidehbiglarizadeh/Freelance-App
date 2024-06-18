a6646fc8258136e568bd4942ff19b8b6122f0903
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-18 17:56:04 +0330

    ProposalTable

--- src/features/project/ProposalsTable.jsx
+++ src/features/project/ProposalsTable.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Empty from "../../ui/Empty";
+import Table from "../../ui/Table";
+import ProposalRow from "./ProposalRow";
 
-function ProposalsTable() {
+function ProposalsTable({ proposals }) {
+
+  if (!proposals.length) return <Empty resourceName="درخواستی یافت نشد" />;
+
   return (
-    <div>ProposalsTable</div>
+    <Table>
+      <Table.Header>
+        <th>#</th>
+        <th>فریلنسر</th>
+        <th>توضیحات</th>
+        <th>زمان تحویل</th>
+        <th>هزینه</th>
+        <th>وضعیت</th>
+        <th>عملیات</th>
+      </Table.Header>
+      <Table.Body>
+        {proposals.map((proposal, index) => (
+          <ProposalRow key={proposal._id} proposal={proposal} index={index} />
+        ))}
+      </Table.Body>
+    </Table>
   )
 }
 
-export default ProposalsTable
+export default ProposalsTable;
