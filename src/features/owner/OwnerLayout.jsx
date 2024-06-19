19d81b7b2c53abb88ab49f5bc09bffdb6e17cf3b
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-19 15:43:40 +0330

    Dynamic AppLayout

---
+++ src/features/owner/OwnerLayout.jsx
@@ -0,0 +1,24 @@
+import { HiCollection, HiHome } from "react-icons/hi";
+import AppLayout from "../../ui/AppLayout";
+import CustomNavLink from "../../ui/CustomNavLink";
+import Sidebar from "../../ui/Sidebar";
+
+function OwnerLayout() {
+  return (
+    <AppLayout>
+      <Sidebar>
+        <CustomNavLink to="/owner/dashboard">
+          <HiHome />
+          <span>داشبورد</span>
+        </CustomNavLink>
+
+        <CustomNavLink to="/owner/projects">
+          <HiCollection />
+          <span>پروژه ها</span>
+        </CustomNavLink>
+      </Sidebar>
+    </AppLayout>
+  );
+}
+
+export default OwnerLayout;
