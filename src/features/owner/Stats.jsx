740818d096547c222707db5228832a08e3091235
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-19 16:31:41 +0330

    Stat component

--- src/features/owner/Stats.jsx
+++ src/features/owner/Stats.jsx
@@ -1,3 +1,10 @@
+import {
+  HiCollection,
+  HiCurrencyDollar,
+  HiOutlineViewGrid,
+} from "react-icons/hi";
+import Stat from "./Stat";
+
 function Stats({ projects }) {
   const numOfProjects = projects.length;
   const numOfAcceptedProjects = projects.map((p) => p.status === 2).length;
@@ -6,7 +13,28 @@ function Stats({ projects }) {
     0
   );
 
-  return <div>Stats</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-x-8">
+      <Stat
+        color="primary"
+        title="پروژه ها"
+        value={numOfProjects}
+        icon={<HiOutlineViewGrid className="w-20 h-20" />}
+      />
+      <Stat
+        color="green"
+        title="پروژه های واگذار شده"
+        value={numOfAcceptedProjects}
+        icon={<HiCurrencyDollar className="w-20 h-20" />}
+      />
+      <Stat
+        color="blue"
+        title="درخواست ها"
+        value={numOfProposals}
+        icon={<HiCollection className="w-20 h-20" />}
+      />
+    </div>
+  );
 }
 
 export default Stats;
