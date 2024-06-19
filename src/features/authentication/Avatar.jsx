648ba11e6c48858242c40a7e8d1a5838a11a7c53
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-19 12:04:59 +0330

    App Header

---
+++ src/features/authentication/Avatar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import useUser from "./useUser";
+
+function Avatar() {
+  const { user, isLoading } = useUser();
+
+  return (
+    <div className="flex items-center gap-x-2 text-secondary-600">
+      <img
+        className="w-7 h-7 rounded-full object-cover object-center"
+        src="/user.jpg"
+        alt="user-account"
+      />
+      <span>{user?.name}</span>
+    </div>
+  );
+}
+
+export default Avatar;
