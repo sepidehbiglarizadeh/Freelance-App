3231eccb6686b4f0fc9b64fbe7142964d0d0c28b
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-20 12:34:44 +0330

    Filter Drop Down

--- src/features/freelancer/projects/ProjectsHeader.jsx
+++ src/features/freelancer/projects/ProjectsHeader.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import useCategories from "../../../hooks/useCategories";
+import FilterDropDown from "../../../ui/FilterDropDown";
 
 function ProjectsHeader() {
+  const { transformedCategories } = useCategories();
+
   return (
     <div className="flex items-center justify-between text-secondary-700 mb-8">
       <h1 className="text-lg font-bold">لیست پروژه ها</h1>
-      <div>فیلترها</div>
+      <div>
+        <FilterDropDown
+          filterField="category"
+          options={[{ value: "All", label: "دسته بندی (همه)" }, ...transformedCategories]}
+        />
+      </div>
     </div>
   );
 }
