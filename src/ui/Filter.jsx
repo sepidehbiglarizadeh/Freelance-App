d98ed62fa816aece1be74a907142417d61bccbd0
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2024-06-22 15:22:14 +0330

    Protected Route

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -12,14 +12,14 @@ function Filter({ filterField, options }) {
   return (
     <div className="flex items-center gap-x-2 text-xs">
       <span>وضعیت</span>
-      <div className="flex items-center gap-x-2 border border-secondary-100 bg-secondary-0 rounded-lg">
+      <div className="flex items-center gap-x-2 p-1 border border-secondary-100 bg-secondary-0 rounded-lg">
         {options.map((item) => {
           const isActive = item.value === currentFilter;
           return (
             <button
               key={item.value}
               disabled={isActive}
-              className={`whitespace-nowrap rounded-md px-4 py-2 font-bold transition-all duration-300 ${
+              className={`whitespace-nowrap rounded-md px-4 py-1 font-bold transition-all duration-300 ${
                 isActive
                   ? "!bg-primary-900 text-white"
                   : "bg-secondary-0 text-secondary-800"
